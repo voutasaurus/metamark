5378fe879242577252c96410e18471acc5dbb784
fourtyfour-saka <sakako@derpymail.com>
2013-09-21 12:22:32 +1000

    Starting work on handlers etc

--- blue.go
+++ blue.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-		"fmt"
-		"html/template"
-		"io/ioutil"
-		"net/http"
-		//"regexp"
-		"strings"
-		//"errors"
+	"fmt"
+	"html/template"
+	//"io/ioutil"
+	"net/http"
+	"regexp"
+	//"strings"
+	//"errors"
 )
 
-type Page struct {
+type Link struct {
+	URL         string
+	Description string
+}
+
+type List struct {
 	Title string
-	Body []byte
+	Body  []Link
 }
 
 /*
@@ -34,18 +39,67 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
 */
 
-func frontHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/view/Front_Page", http.StatusFound)
+func loadList(key string) (*List, error) {
+	// implement later
+	url := "html://www.wikipedia.com"
+	desc := "Wikipedia: The Commie's Encyclopedia"
+	link := Link{URL: url, Description: desc}
+	list := []Link{link} // list := make([]Link, 1) ; append(list, link)
+	ret := &List{Title: key, Body: list}
+
+	return ret, nil
 }
 
+const lenPath = len("/new/")
+
+var titleValidator = regexp.MustCompile("^[a-zA-Z_]+$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		title := r.URL.Path[lenPath:]
+		if !titleValidator.MatchString(title) {
+			http.NotFound(w, r)
+			return //err = errors.New("Invalid Page Title")
+		}
+		fn(w, r, title)
+	}
+}
+
+func frontHandler(w http.ResponseWriter, r *http.Request) {
+	//http.Redirect(w, r, "/index.html", http.StatusFound)
+	renderTemplate(w, "index", nil)
+}
+
+/*
 func newHandler(w http.ResponseWriter, r *http.Request) {
 	p, err := loadPage()
 	if err != nil {
 		p = &Page{Title: title}
 	}
 	renderTemplate(w, "new", p)
+}
+*/
+
+func oldHandler(w http.ResponseWriter, r *http.Request, key string) {
+	l, err := loadList(key)
+	if err != nil {
+		l = &List{}
+	}
+	renderTemplate(w, "old", l)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index", nil)
 }
 
 /* // Activate for functions on templates
@@ -56,7 +110,7 @@ var funcMap = template.FuncMap{
 var templates = template.Must(template.New("titleTest").Funcs(funcMap).ParseFiles("tmpl/edit.html", "tmpl/view.html"))
 */
 
-var templates = template.Must(template.ParseFiles("tmpl/main.html", "tmpl/newlist.html"))
+var templates = template.Must(template.ParseFiles("www/index.html", "www/new.html", "www/old.html"))
 
 /* // Parts may be salvagable for newlist form action
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -72,29 +126,27 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 
 */
 
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+func renderTemplate(w http.ResponseWriter, tmpl string, l *List) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", l)
 	if err != nil {
 		fmt.Println("Template " + tmpl + " cannot be rendered")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-
 func main() {
 	http.HandleFunc("/", frontHandler)
-  
-  	http.HandleFunc("/new/", newHandler)
-  
+	http.HandleFunc("/index.html", indexHandler)
+	// http.HandleFunc("/new/", newHandler) // Don't register yet, not implemented
+	http.HandleFunc("/old", makeHandler(oldHandler))
+
 	http.ListenAndServe(":8080", nil)
 	fmt.Println("test")
 }
 
-/*	
+/*
 	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
 	p1.save()
 	p2, _ := loadPage("TestPage")
 	fmt.Println(string(p2.Body))
 */
-
