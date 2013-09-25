a7e90d53db0f97a16fbc981ce8b41aab29985c75
voutasaurus <voutasaurus@gmail.com>
2013-09-25 22:04:33 +1000

    Added indentation in docs.go for models Indentation of code makes godoc recognise code and format it appropriately

--- Models/doc.go
+++ Models/doc.go
@@ -3,10 +3,10 @@ The models package provides a means of communicating with the database
 
 In your main function Initialise:
 
-getList := make(chan ListRetrieve)
-addList := make(chan AddRequest)
-removeList := make(chan string)
-go BookmarksCollection(getList, addList, removeList)
+	getList := make(chan ListRetrieve)
+	addList := make(chan AddRequest)
+	removeList := make(chan string)
+	go BookmarksCollection(getList, addList, removeList)
 
 This runs the bookmarks collection and provides channels for requests.
 
@@ -14,22 +14,22 @@ To make requests of the database, do the following:
 
 1. Retrieve a list
 
-key := keyToRetrieve
-reply := make(chan Bookmarks)
-getList <- ListRetrieve{key, reply}
-newList := <- reply
+	key := keyToRetrieve
+	reply := make(chan Bookmarks)
+	getList <- ListRetrieve{key, reply}
+	newList := <- reply
 
 2. Add a list
 
-bookmarks := bookmarksToInsert
-reply := make(chan string)
-addList <- AddRequest{bookmarks, reply}
-newKey := <- reply
+	bookmarks := bookmarksToInsert
+	reply := make(chan string)
+	addList <- AddRequest{bookmarks, reply}
+	newKey := <- reply
 
 3. Remove a list
 
-key := keyToDelete
-removeList <- key
+	key := keyToDelete
+	removeList <- key
 
 */
 package models
